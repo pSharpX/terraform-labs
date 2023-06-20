7d92094e91d06af7ff680766370036d81d9fa491
psharpx <crivera2093@gmail.com>
2023-06-19 23:56:57 -0500

    fix(argocd): change argocd installation

--- aforo255-nodejs/scripts/argocd.tf
+++ aforo255-nodejs/scripts/argocd.tf
@@ -12,15 +12,25 @@ data "http" "argocd-resource-manifest" {
     url = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
 }
 
-resource "local_file" "argocd-manifest" {
+resource "local_file" "argocd-manifest-file" {
     content = "${data.http.argocd-resource-manifest.response_body}"
     filename = "./configs/argocd-install.yaml"
 }
 
-resource "kubectl_manifest" "argocd" {
-    yaml_body = "${local_file.argocd-manifest.content}"
+data "kubectl_file_documents" "argocd-manifests" {
+    content = file("${local_file.argocd-manifest-file.filename}")
 
     depends_on = [ 
-        local_file.argocd-manifest
+        local_file.argocd-manifest-file
+     ]
+}
+
+resource "kubectl_manifest" "argocd" {
+    for_each  = data.kubectl_file_documents.argocd-manifests.manifests
+    yaml_body = each.value
+    override_namespace = "${kubernetes_namespace.argocd_ns.metadata[0].name}"
+
+    depends_on = [ 
+        local_file.argocd-manifest-file
      ]
 }

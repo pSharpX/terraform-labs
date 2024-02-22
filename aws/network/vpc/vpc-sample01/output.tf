bc3d4a0f9f93abf05298b3dd86e954681e2c5dcb
Christian Rivera <crivera2093@gmail.com>
2024-02-22 00:44:07 -0500

    fix: changed ouput for vpc sample

--- aws/network/vpc/vpc-sample01/output.tf
+++ aws/network/vpc/vpc-sample01/output.tf
@@ -1,3 +1,18 @@
-output "vpc-subnets" {
+output "vpc-id" {
+    value = aws_vpc.vpc-sample01.id
+}
+
+output "vpc-arn" {
     value = aws_vpc.vpc-sample01.arn
+}
+
+output "vpc-subnets" {
+    value = {
+        "main-public-1" = aws_subnet.main-public-1.id
+        "main-public-2" = aws_subnet.main-public-2.id
+        "main-public-3" = aws_subnet.main-public-3.id
+        "main-private-1" = aws_subnet.main-private-1.id
+        "main-private-2" = aws_subnet.main-private-2.id
+        "main-private-3" = aws_subnet.main-private-3.id
+    }
 }

06a494eef88feae64232d051b83900cdd55955e8
Christian Rivera <crivera2093@gmail.com>
2024-02-29 18:23:02 -0500

    feat: added path in groups for better organization

--- aws/iam/policy-attachments/groups.tf
+++ aws/iam/policy-attachments/groups.tf
@@ -1,12 +1,15 @@
 resource "aws_iam_group" "developers" {
     name = var.DEVELOPER_GROUP
+    path = "/users/developer"
 }
 
 resource "aws_iam_group" "administrators" {
     name = var.ADMINISTRATOR_GROUP
+    path = "/users/administrator"
 }
 
 resource "aws_iam_group" "external_groups" {
     for_each = var.GROUPS
     name = each.value
+    path = "/users/external"
 }

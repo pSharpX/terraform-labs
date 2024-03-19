4e33c8d442d50b907f1f4e89b5bb8734aefa2d9f
Christian Rivera <crivera2093@gmail.com>
2024-03-18 20:16:18 -0500

    feat: aws vpc module

---
+++ aws/modules/vpc/vars.tf
@@ -0,0 +1,47 @@
+variable "AWS_REGION" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "AWS_ACCESS_KEY" {
+    type = string
+}
+
+variable "AWS_SECRET_KEY" {
+    type = string  
+}
+
+variable "APPLICATION_ID" {
+    default = "onebank"
+    type = string
+    description = "Application Identifier"
+}
+
+variable "ENVIRONMENT" {
+    default = "dev"
+    type = string
+    description = "Environment where resources are provisioned"
+
+    validation {
+      condition = contains(["dev", "uat", "staging", "prod"], var.ENVIRONMENT)
+      error_message = "Allowed values for ENVIRONMENT are [dev, uat, staging, prod]"
+    }
+}
+
+variable "TECHNICAL_OWNER" {
+    default = "TeamGOAT"
+    type = string
+    description = "Team responsable for application support"
+}
+
+variable "PROVISIONER" {
+    default = "Terraform"
+    type = string
+    description = "Tool used to provision resources"  
+}
+
+variable "DATA_CLASSIFICATION" {
+    default = "Restricted"
+    type = string
+    description = "A classification for the type of data managed by resources"
+}

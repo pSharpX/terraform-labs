05439bcc0d8e6c07fa1f97a662bb4e70b6941040
Christian Rivera <crivera2093@gmail.com>
2024-03-18 20:13:14 -0500

    fix: changed vpc subnets ouput

--- aws/network/vpc/vpc-sample01/output.tf
+++ aws/network/vpc/vpc-sample01/output.tf
@@ -6,11 +6,16 @@ output "vpc-arn" {
     value = aws_vpc.vpc-sample01.arn
 }
 
-output "vpc-subnets" {
+output "public-subnets" {
     value = {
         "main-public-1" = aws_subnet.main-public-1.id
         "main-public-2" = aws_subnet.main-public-2.id
         "main-public-3" = aws_subnet.main-public-3.id
+    }
+}
+
+output "private-subnets" {
+    value = {
         "main-private-1" = aws_subnet.main-private-1.id
         "main-private-2" = aws_subnet.main-private-2.id
         "main-private-3" = aws_subnet.main-private-3.id

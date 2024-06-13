5d20ae942dd529ceccaedd749357cd82ac827829
Christian Rivera <crivera2093@gmail.com>
2024-06-13 16:15:55 -0500

    feat(GCP): added additional ip range in firewall rule for ALB healthcheck systems.

--- gcp/networking/firewall/healthcheck-rules/firewall.tf
+++ gcp/networking/firewall/healthcheck-rules/firewall.tf
@@ -1,5 +1,6 @@
 ## allows ingress traffic from the IP range "130.211.0.0/22", "35.191.0.0/16". This range contains all IP addresses for health check.
 ## https://cloud.google.com/load-balancing/docs/health-check-concepts#ip-ranges
+## ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
 
 resource "google_compute_firewall" "allow_health_check_lb_probers" {
     name = "${local.applicationId}-allow-health-check-lb"

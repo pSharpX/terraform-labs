fecda4a5926b88b814f74134f4f7375262d082fe
Christian Rivera <crivera2093@gmail.com>
2024-06-13 19:11:06 -0500

    feat(GCP): added new IP Address Range for Health Check System in firewall rule.

--- gcp/networking/firewall/healthcheck-rules/firewall.tf
+++ gcp/networking/firewall/healthcheck-rules/firewall.tf
@@ -1,6 +1,7 @@
 ## allows ingress traffic from the IP range "130.211.0.0/22", "35.191.0.0/16". This range contains all IP addresses for health check.
 ## https://cloud.google.com/load-balancing/docs/health-check-concepts#ip-ranges
-## ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
+## [ "130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20" ]
+## [ "130.211.0.0/22", "35.191.0.0/16" ]
 
 resource "google_compute_firewall" "allow_health_check_lb_probers" {
     name = "${local.applicationId}-allow-health-check-lb"
@@ -8,7 +9,7 @@ resource "google_compute_firewall" "allow_health_check_lb_probers" {
     description = "Allow inbound traffic from health check (LB)"
     direction = "INGRESS"
     priority = 65534
-    source_ranges = [ "130.211.0.0/22", "35.191.0.0/16" ] ## allow all access from health check ranges
+    source_ranges = [ "130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20" ] ## allow all access from health check ranges
     disabled = false
 
     allow {

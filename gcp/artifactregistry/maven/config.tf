35805932169db0c57a43aff047daf4d0203ae366
Christian Rivera <crivera2093@gmail.com>
2024-07-24 11:39:07 -0500

    fix(GCP): add gradle configuration template files for publishing artifact to Artifact Registry.

---
+++ gcp/artifactregistry/maven/config.tf
@@ -0,0 +1,14 @@
+data "template_file" "gradleconfig" {
+    template = file("./templates/gradle.properties.tmpl")
+
+    vars = {
+        artifactory_contextUrl = "https://${google_artifact_registry_repository.onebank_repository.location}-maven.pkg.dev/${google_artifact_registry_repository.onebank_repository.project}/${google_artifact_registry_repository.onebank_repository.name}"
+        artifactory_user = "_json_key_base64"
+        artifactory_password = google_service_account_key.global_artifact_key.private_key
+    }
+}
+
+resource "local_file" "gradleconfig" {
+    content = data.template_file.gradleconfig.rendered
+    filename = "./configs/gradle.properties"
+}

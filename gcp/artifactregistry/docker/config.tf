7f509f31d02fe8cbfc4bd5b337403798291752e8
Christian Rivera <crivera2093@gmail.com>
2024-07-26 11:38:36 -0500

    feat(GCP): add template for Docker config.

--- gcp/artifactregistry/docker/config.tf
+++ gcp/artifactregistry/docker/config.tf
@@ -27,3 +27,18 @@ resource "local_file" "login_service_account" {
     content = data.template_file.login_service_account.rendered
     filename = "./configs/login-service-account.credential.sh"
 }
+
+data "template_file" "login_auth_config" {
+    template = file("./templates/config.json.tmpl")
+
+    vars = {
+        repository = "${google_artifact_registry_repository.onebank_repository.location}-docker.pkg.dev"
+        auth_token = base64encode("oauth2accesstoken:${data.google_service_account_access_token.global.access_token}")
+    }
+}
+
+resource "local_file" "login_auth_config" {
+    content = data.template_file.login_auth_config.rendered
+    filename = "./configs/config.credential.json"
+}
+

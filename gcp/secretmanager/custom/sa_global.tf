c464e32c0cab82b398f77f59e63396ce1208138d
Christian Rivera <crivera2093@gmail.com>
2024-08-03 00:43:04 -0500

    feat(GCP): create service account  with Secret Manager Accessor permission.

---
+++ gcp/secretmanager/custom/sa_global.tf
@@ -0,0 +1,32 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/secret_manager_secret_iam
+
+resource "google_service_account" "global_secret_sa" {
+    project = data.google_project.onebank.project_id
+    account_id = "global-secret-sa"
+    display_name = "Global Secret Service Account"
+    description = "Global Service Account for Cloud Secret Manager"
+}
+
+# Roles
+# roles/secretmanager.secretAccessor
+
+resource "google_project_iam_member" "secret_accessor" {
+    project = data.google_project.onebank.project_id
+    role = "roles/secretmanager.secretAccessor"
+    member = "serviceAccount:${google_service_account.global_secret_sa.email}"
+}
+
+resource "google_service_account_key" "global_secret_sa_key" {
+    service_account_id = google_service_account.global_secret_sa.name
+}
+
+resource "local_file" "global_account_credentials" {
+    content = base64decode(google_service_account_key.global_secret_sa_key.private_key)
+    filename = "./configs/global-secret-sa.json"
+}
+
+data "google_service_account_access_token" "global_sa_access_token" {
+    target_service_account = google_service_account.global_secret_sa.email
+    lifetime = "1800s"
+    scopes = [ "cloud-platform", "userinfo-email" ]
+}
---
+++ gcp/secretmanager/custom/sa_secret_specific.tf
@@ -0,0 +1,33 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/secret_manager_secret_iam
+
+resource "google_service_account" "secret_specific_sa" {
+    project = data.google_project.onebank.project_id
+    account_id = "secret-specific-sa"
+    display_name = "Secret-Specific Service Account"
+    description = "Service Account for Cloud Secret Manager (Secret-Specific)"
+}
+
+# Roles
+# roles/secretmanager.secretAccessor
+
+resource "google_secret_manager_secret_iam_member" "secret_accessor" {
+    project = data.google_project.onebank.project_id
+    secret_id = google_secret_manager_secret.db_password.id
+    role = "roles/secretmanager.secretAccessor"
+    member = "serviceAccount:${google_service_account.secret_specific_sa.email}"
+}
+
+resource "google_service_account_key" "secret_specific_sa_key" {
+    service_account_id = google_service_account.secret_specific_sa.name
+}
+
+resource "local_file" "secret_specific_account_credentials" {
+    content = base64decode(google_service_account_key.secret_specific_sa_key.private_key)
+    filename = "./configs/secret-specific-sa.json"
+}
+
+data "google_service_account_access_token" "secret_specific_sa_access_token" {
+    target_service_account = google_service_account.secret_specific_sa.email
+    lifetime = "1800s"
+    scopes = [ "cloud-platform", "userinfo-email" ]
+}

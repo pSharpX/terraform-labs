114c2819a70f584f185198761a192bd37f7862cb
Christian Rivera <crivera2093@gmail.com>
2024-09-24 12:41:18 -0500

    fix(GCP): rollback changes in ACL.

--- gcp/cloudstorage/accesscontrol/acl/objects_acl.tf
+++ gcp/cloudstorage/accesscontrol/acl/objects_acl.tf
@@ -1,18 +1,18 @@
 # https://cloud.google.com/storage/docs/access-control
 # https://cloud.google.com/storage/docs/access-control/lists
 
-# resource "google_storage_object_access_control" "photo_reader_allUsers" {
-#     bucket = google_storage_bucket.assets.name
-#     object = google_storage_bucket_object.assets_photo.output_name
-#     role = "READER"
-#     entity = "allUsers"
-# }
-
-resource "google_storage_object_acl" "photo_reader_allUsers" {
+resource "google_storage_object_access_control" "photo_reader_allUsers" {
     bucket = google_storage_bucket.assets.name
     object = google_storage_bucket_object.assets_photo.output_name
-
-    role_entity = [ 
-        "READER:allUsers"
-     ]
+    role = "READER"
+    entity = "allUsers"
 }
+
+# resource "google_storage_object_acl" "photo_reader_allUsers" {
+#     bucket = google_storage_bucket.assets.name
+#     object = google_storage_bucket_object.assets_photo.output_name
+
+#     role_entity = [ 
+#         "READER:allUsers"
+#      ]
+# }

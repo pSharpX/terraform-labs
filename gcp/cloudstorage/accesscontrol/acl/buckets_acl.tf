707ca1a05f15318afc828f54488cc4aa7e768dc8
Christian Rivera <crivera2093@gmail.com>
2024-09-24 12:31:04 -0500

    fix(GCP): authoritatively manages the access control list (ACL).

--- gcp/cloudstorage/accesscontrol/acl/buckets_acl.tf
+++ gcp/cloudstorage/accesscontrol/acl/buckets_acl.tf
@@ -2,8 +2,15 @@
 # https://cloud.google.com/storage/docs/access-control
 # https://cloud.google.com/storage/docs/access-control/lists
 
-resource "google_storage_bucket_access_control" "web_reader_allUsers" {
+# resource "google_storage_bucket_access_control" "web_reader_allUsers" {
+#     bucket = google_storage_bucket.web.name
+#     role = "READER"
+#     entity = "allUsers"
+# }
+
+resource "google_storage_bucket_acl" "web_reader_allUsers" {
     bucket = google_storage_bucket.web.name
-    role = "READER"
-    entity = "allUsers"
+    role_entity = [ 
+        "READER:allUsers"
+     ]
 }

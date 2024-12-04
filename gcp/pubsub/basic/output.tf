33cd6ce5d119b4a292335356763b7d6a6103266f
Christian Rivera <crivera2093@gmail.com>
2024-12-04 15:30:41 -0500

    feat(GCP): add output vars for Cloud PubSub resource.

--- gcp/pubsub/basic/output.tf
+++ gcp/pubsub/basic/output.tf
@@ -1,3 +1,19 @@
 output "project_number" {
     value = data.google_project.onebank.number
 }
+
+output "topic_name" {
+    value = google_pubsub_topic.onebank_topic.name
+}
+
+output "topic_id" {
+    value = google_pubsub_topic.onebank_topic.id
+}
+
+output "subscription_name" {
+    value = google_pubsub_subscription.email_notification_subscription.name
+}
+
+output "subscription_id" {
+    value = google_pubsub_subscription.email_notification_subscription.id
+}

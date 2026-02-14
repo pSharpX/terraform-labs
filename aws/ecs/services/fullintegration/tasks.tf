a196eea6cf8af9de385582536bc23e9860a31f2e
Christian Rivera <crivera2093@gmail.com>
2026-02-14 00:51:01 -0500

    feat: set  health check for task definition

--- aws/ecs/services/fullintegration/tasks.tf
+++ aws/ecs/services/fullintegration/tasks.tf
@@ -29,6 +29,13 @@ resource "aws_ecs_task_definition" "application_task" {
           value = var.APPLICATION_NAME
         }
       ]
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost/ || exit 1"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 10
+      }
     }
   ])
 
